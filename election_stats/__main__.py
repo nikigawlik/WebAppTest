83194bc114bf589a993da74c82b13f03e65e5ce1
Nils Gawlik <nilsgawlik@gmx.de>
2019-05-09 15:10:34 +0200

    Convert indentation to spaces in main file

--- election_stats/__main__.py
+++ election_stats/__main__.py
@@ -15,44 +15,44 @@ Session = sessionmaker(bind=engine)
 
 @app.route('/')
 def hello():
-	engine = get_engine()
-	session = Session()
-	states = session.query(Region).filter_by(regionType = RegionType.state).order_by(Region.name).all()
-	session.commit()
-	
-	# we render the states server side, so that they appear instantaneously
-	return render_template('main.html', states = states)
+    engine = get_engine()
+    session = Session()
+    states = session.query(Region).filter_by(regionType = RegionType.state).order_by(Region.name).all()
+    session.commit()
+    
+    # we render the states server side, so that they appear instantaneously
+    return render_template('main.html', states = states)
 
 @app.route('/regions/', methods=['GET'])
 def get_regions():
-	session = Session()
-	regions = session.query(Region).all()
-	return jsonify([r.to_dict() for r in regions])
+    session = Session()
+    regions = session.query(Region).all()
+    return jsonify([r.to_dict() for r in regions])
 
 @app.route('/regions/<region_id>/', methods=['GET'])
 def get_region(region_id):
-	session = Session()
-	region = session.query(Region).filter_by(id = region_id).one()
-	return jsonify(region.to_dict())
+    session = Session()
+    region = session.query(Region).filter_by(id = region_id).one()
+    return jsonify(region.to_dict())
 
 @app.route('/regions/<region_id>/subregions', methods=['GET'])
 def get_subregions(region_id):
-	region_id = int(region_id)
-	session = Session()
-	subregions = session.query(Region).filter_by(parent_region_id = region_id).all()
-	return jsonify([r.to_dict() for r in subregions])
+    region_id = int(region_id)
+    session = Session()
+    subregions = session.query(Region).filter_by(parent_region_id = region_id).all()
+    return jsonify([r.to_dict() for r in subregions])
 
 @app.route('/regions/<region_id>/results', methods=['GET'])
 def get_results(region_id):
-	session = Session()
-	region = session.query(Region).filter_by(id = region_id).one()
-	results = region.results
-	return jsonify([r.to_dict() for r in results])
+    session = Session()
+    region = session.query(Region).filter_by(id = region_id).one()
+    results = region.results
+    return jsonify([r.to_dict() for r in results])
 
 @app.route('/parties/', methods=['GET'])
 def get_parties():
-	session = Session()
-	parties = session.query(Party).all()
-	return jsonify([p.to_dict() for p in parties])
-	
+    session = Session()
+    parties = session.query(Party).all()
+    return jsonify([p.to_dict() for p in parties])
+    
 app.run(debug = True)
