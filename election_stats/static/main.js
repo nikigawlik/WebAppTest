2432f12715842fa9ac97f203157e803fd01b9152
Nils Gawlik <nilsgawlik@gmx.de>
2019-05-14 16:52:36 +0200

    Fix charts not updating correctly.

--- election_stats/static/main.js
+++ election_stats/static/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var chart1, chart2, chart3;
+
 window.onload = function() {
     var app = new Vue({
         delimiters:['${', '}'],
@@ -45,11 +47,73 @@ window.onload = function() {
             },
             getResults (districtID) {
                 this.currentDistrict = this.districts.find((district) => (district.id == districtID));
-                var erststimmenChart = [];
-                var zweitstimmenChart = [];
-                var erstzweitstimmenChart = [];
                 console.log(`Fetch results for ${districtID}...`)
                 let self = this;
+
+                // console.dir("Erstzweitstimmen: "+chart1.data.datasets[0].data.toString());
+                var ctx1 = document.getElementById('bar').getContext('2d');
+                if(chart1) chart1.destroy();
+                chart1 = new Chart(ctx1, {
+                    // The type of chart we want to create
+                    type: 'bar',
+            
+                    // The data for our dataset
+                    data: {
+                        labels: ['SDP Erststimme','SDP Zweitstimme', 'LINKE Erststimme','LINKE Zweitstimme', 'CDU Erststimme', 'CDU Zweitstimme', 'Grüne Erststimme', 'Grüne Zweitstimme', 'AfD Erststimme', 'AfD Zweitstimme', 'FDP Erststimme', 'FDP Zweitstimme', 'Andere Zweitstimme', 'Andere Zweitstimme'],
+                        datasets: [{
+                            label: 'Erst- und Zweitstimmenverteilung',
+                            backgroundColor:['red','red','purple','purple','black','black','green','green', 'blue', 'blue','yellow','yellow','grey','grey'],
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                        }]
+                    },
+            
+                    // Configuration options go here
+                    options: {}
+                });
+
+                var ctx2 = document.getElementById('pie1').getContext('2d');
+                if(chart2) chart2.destroy();
+                chart2 = new Chart(ctx2, {
+                    // The type of chart we want to create
+                    type: 'pie',
+            
+                    // The data for our dataset
+                    data: {
+                        labels: ['SDP', 'LINKE', 'CDU', 'Grüne', 'AfD', 'FDP', 'Andere'],
+                        datasets: [{
+                            label: 'Erststimmenverteilung',
+                            backgroundColor:['red','purple','black','green', 'blue','yellow','grey'],
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [0, 0, 0, 0, 0, 0, 0]
+                        }]
+                    },
+            
+                    // Configuration options go here
+                    options: {}
+                });
+
+                var ctx3 = document.getElementById('pie2').getContext('2d');
+                if(chart3) chart3.destroy();
+                chart3 = new Chart(ctx3, {
+                    // The type of chart we want to create
+                    type: 'pie',
+            
+                    // The data for our dataset
+                    data: {
+                        labels: ['SDP', 'LINKE', 'CDU', 'Grüne', 'AfD', 'FDP', 'Andere'],
+                        datasets: [{
+                            label: 'Zweitstimmenverteilung',
+                            backgroundColor:['red','purple','black','green', 'blue','yellow','grey'],
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: [0, 0, 0, 0, 0, 0, 0]
+                        }]
+                    },
+            
+                    // Configuration options go here
+                    options: {}
+                });
+
                 axios.get(`/regions/${districtID}/results`).then(function(response){
                     let district = self.districts
                     self.results = response.data;
@@ -59,116 +123,59 @@ window.onload = function() {
                         r.zweitstimmenRelative = r.zweitstimmen / self.currentDistrict.voters;
                        
                         if (r.party_id == 1){
-                            erststimmenChart[0] = r.erststimmen;
-                            zweitstimmenChart[0] = r.zweitstimmen;
-                            erstzweitstimmenChart[0] = r.erststimmen;
-                            erstzweitstimmenChart[1] = r.zweitstimmen;
+                            chart2.data.datasets[0].data[0] = r.erststimmen;
+                            chart3.data.datasets[0].data[0] = r.zweitstimmen;
+                            chart1.data.datasets[0].data[0] = r.erststimmen;
+                            chart1.data.datasets[0].data[1] = r.zweitstimmen;
 
                         }
                         if (r.party_id == 2){
-                            erststimmenChart[1] = r.erststimmen;
-                            zweitstimmenChart[1] = r.zweitstimmen;
-                            erstzweitstimmenChart[2] = r.erststimmen;
-                            erstzweitstimmenChart[3] = r.zweitstimmen;
+                            chart2.data.datasets[0].data[1] = r.erststimmen;
+                            chart3.data.datasets[0].data[1] = r.zweitstimmen;
+                            chart1.data.datasets[0].data[2] = r.erststimmen;
+                            chart1.data.datasets[0].data[3] = r.zweitstimmen;
                         }
                         if (r.party_id == 0){
-                            erststimmenChart[2] = r.erststimmen;
-                            zweitstimmenChart[2] = r.zweitstimmen;
-                            erstzweitstimmenChart[4] = r.erststimmen;
-                            erstzweitstimmenChart[5] = r.zweitstimmen;
+                            chart2.data.datasets[0].data[2] = r.erststimmen;
+                            chart3.data.datasets[0].data[2] = r.zweitstimmen;
+                            chart1.data.datasets[0].data[4] = r.erststimmen;
+                            chart1.data.datasets[0].data[5] = r.zweitstimmen;
                         }
                         if (r.party_id == 3){
-                            erststimmenChart[3] = r.erststimmen;
-                            zweitstimmenChart[3] = r.zweitstimmen;
-                            erstzweitstimmenChart[6] = r.erststimmen;
-                            erstzweitstimmenChart[7] = r.zweitstimmen;
+                            chart2.data.datasets[0].data[3] = r.erststimmen;
+                            chart3.data.datasets[0].data[3] = r.zweitstimmen;
+                            chart1.data.datasets[0].data[6] = r.erststimmen;
+                            chart1.data.datasets[0].data[7] = r.zweitstimmen;
                         }
                         if (r.party_id == 6){
-                            erststimmenChart[4] = r.erststimmen;
-                            zweitstimmenChart[4] = r.zweitstimmen;
-                            erstzweitstimmenChart[8] = r.erststimmen;
-                            erstzweitstimmenChart[9] = r.zweitstimmen;
+                            chart2.data.datasets[0].data[4] = r.erststimmen;
+                            chart3.data.datasets[0].data[4] = r.zweitstimmen;
+                            chart1.data.datasets[0].data[8] = r.erststimmen;
+                            chart1.data.datasets[0].data[9] = r.zweitstimmen;
                         }
                         if (r.party_id == 5){
-                            erststimmenChart[5] = r.erststimmen;
-                            zweitstimmenChart[5] = r.zweitstimmen;
-                            erstzweitstimmenChart[10] = r.erststimmen;
-                            erstzweitstimmenChart[11] = r.zweitstimmen;
+                            chart2.data.datasets[0].data[5] = r.erststimmen;
+                            chart3.data.datasets[0].data[5] = r.zweitstimmen;
+                            chart1.data.datasets[0].data[10] = r.erststimmen;
+                            chart1.data.datasets[0].data[11] = r.zweitstimmen;
                         }
                         if (r.party_id >= 7 || r.party_id == 4) {
-                            erststimmenChart[6] = (erststimmenChart[6] || 0) + Number(r.erststimmen);
-                            zweitstimmenChart[6] = (zweitstimmenChart[6] || 0) + Number(r.zweitstimmen);
-                            erstzweitstimmenChart[12] = (erstzweitstimmenChart[12] || 0) + Number(r.erststimmen);
-                            erstzweitstimmenChart[13] = (erstzweitstimmenChart[13] || 0) + Number(r.zweitstimmen);
+                            chart2.data.datasets[0].data[6] = (chart2.data.datasets[0].data[6] || 0) + Number(r.erststimmen);
+                            chart3.data.datasets[0].data[6] = (chart3.data.datasets[0].data[6] || 0) + Number(r.zweitstimmen);
+                            chart1.data.datasets[0].data[12] = (chart1.data.datasets[0].data[12] || 0) + Number(r.erststimmen);
+                            chart1.data.datasets[0].data[13] = (chart1.data.datasets[0].data[13] || 0) + Number(r.zweitstimmen);
                         }
 
                         
                     })
 
-                    console.dir("Erstzweitstimmen: "+erstzweitstimmenChart.toString());
-                    var ctx1 = document.getElementById('bar').getContext('2d');
-                    var chart1 = new Chart(ctx1, {
-                        // The type of chart we want to create
-                        type: 'bar',
-                
-                        // The data for our dataset
-                        data: {
-                            labels: ['SDP Erststimme','SDP Zweitstimme', 'LINKE Erststimme','LINKE Zweitstimme', 'CDU Erststimme', 'CDU Zweitstimme', 'Grüne Erststimme', 'Grüne Zweitstimme', 'AfD Erststimme', 'AfD Zweitstimme', 'FDP Erststimme', 'FDP Zweitstimme', 'Andere Zweitstimme', 'Andere Zweitstimme'],
-                            datasets: [{
-                                label: 'Erst- und Zweitstimmenverteilung',
-                                backgroundColor:['red','red','purple','purple','black','black','green','green', 'blue', 'blue','yellow','yellow','grey','grey'],
-                                borderColor: 'rgb(255, 99, 132)',
-                                data: erstzweitstimmenChart
-                            }]
-                        },
-                
-                        // Configuration options go here
-                        options: {}
-                    });
-    
-                    var ctx2 = document.getElementById('pie1').getContext('2d');
-                    var chart2 = new Chart(ctx2, {
-                        // The type of chart we want to create
-                        type: 'pie',
-                
-                        // The data for our dataset
-                        data: {
-                            labels: ['SDP', 'LINKE', 'CDU', 'Grüne', 'AfD', 'FDP', 'Andere'],
-                            datasets: [{
-                                label: 'Erststimmenverteilung',
-                                backgroundColor:['red','purple','black','green', 'blue','yellow','grey'],
-                                borderColor: 'rgb(255, 99, 132)',
-                                data: erststimmenChart
-                            }]
-                        },
-                
-                        // Configuration options go here
-                        options: {}
-                    });
-
-                    var ctx3 = document.getElementById('pie2').getContext('2d');
-                    var chart3 = new Chart(ctx3, {
-                        // The type of chart we want to create
-                        type: 'pie',
-                
-                        // The data for our dataset
-                        data: {
-                            labels: ['SDP', 'LINKE', 'CDU', 'Grüne', 'AfD', 'FDP', 'Andere'],
-                            datasets: [{
-                                label: 'Zweitstimmenverteilung',
-                                backgroundColor:['red','purple','black','green', 'blue','yellow','grey'],
-                                borderColor: 'rgb(255, 99, 132)',
-                                data: zweitstimmenChart
-                            }]
-                        },
-                
-                        // Configuration options go here
-                        options: {}
-                    });
     
                     document.getElementById("pie-chart").style.display = "block";
                     document.getElementById("bar-chart").style.display = "block";
 
+                    chart1.update();
+                    chart2.update();
+                    chart3.update();
 
                 }, function() {
                     console.log("Could not fetch results");
